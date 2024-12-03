1354a2af581a0b2f249aa14a7e6c9653dd1d7f2e
DuckBroApprentice <pigbirthin0320@gmail.com>
2024-12-03 16:10:53 +0800

    修改bug

--- CloudRestaurant/main.go
+++ CloudRestaurant/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/DuckBroApprentice/Shopping/CloudRestaurant/controller"
 	"github.com/DuckBroApprentice/Shopping/CloudRestaurant/tool"
 	"github.com/gin-gonic/gin"
 )
@@ -14,6 +15,13 @@ func main() {
 
 	app := gin.Default()
 
+	registerRouter(app)
+
 	//app.json中app_host,app_port
 	app.Run(cfg.AppHost + ":" + cfg.AppPort)
+}
+
+// 路由設置
+func registerRouter(router *gin.Engine) {
+	new(controller.HelloController).Router(router)
 }
