8b7ddb691be18aecb8651b864150bd1ca31d2337
DuckBroApprentice <pigbirthin0320@gmail.com>
2024-12-03 16:10:10 +0800

    設置路由

---
+++ CloudRestaurant/controller/HelloController.go
@@ -0,0 +1,14 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type HelloController struct{}
+
+func (hello *HelloController) Router(engine *gin.Engine) {
+	engine.GET("/hello", hello.Hello)
+}
+
+//解析 /hello
+func (hello *HelloController) Hello(context *gin.Context) {
+
+}
