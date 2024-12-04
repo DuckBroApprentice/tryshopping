38e70c4ceaaf42377f0cdf3c0d53752f0c76ebc2
DuckBroApprentice <pigbirthin0320@gmail.com>
2024-12-04 14:47:31 +0800

    新增映射數據庫表單

--- CloudRestaurant/main.go
+++ CloudRestaurant/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/DuckBroApprentice/Shopping/CloudRestaurant/controller"
 	"github.com/DuckBroApprentice/Shopping/CloudRestaurant/tool"
 	"github.com/gin-gonic/gin"
@@ -11,6 +13,12 @@ func main() {
 	cfg, err := tool.ParseConfig("./config/app.json")
 	if err != nil {
 		panic(err.Error())
+	}
+
+	_, err = tool.OrmEngine(cfg)
+	if err != nil {
+		log.Fatal(err.Error())
+		return
 	}
 
 	app := gin.Default()
@@ -24,4 +32,6 @@ func main() {
 // 路由設置
 func registerRouter(router *gin.Engine) {
 	new(controller.HelloController).Router(router)
+	new(controller.HelloController).Router(router)
+	new(controller.MemberController).Router(router)
 }
--- CloudRestaurant/tool/OrmEngine.go
+++ CloudRestaurant/tool/OrmEngine.go
@@ -2,6 +2,7 @@ package tool
 
 import (
 	"github.com/DuckBroApprentice/Shopping/CloudRestaurant/model"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
 )
 
@@ -14,21 +15,21 @@ type Orm struct {
 func OrmEngine(cfg *Config) (*Orm, error) {
 	database := cfg.Database
 	conn := database.User + ":" + database.Password + "@tcp(" + database.Host + ":" + database.Port + ")/" + database.DbName + "?charset=" + database.Charset
-	engine, err := xorm.NewEngine("msysql", conn)
+	engine, err := xorm.NewEngine(database.Driver, conn)
 
 	if err != nil {
 		return nil, err
 	}
 	engine.ShowSQL(database.ShowSql)
 
-	err = engine.Sync2(new(model.SmsCode))
+	//映射成數據庫當中的表單
+	err = engine.Sync2(new(model.SmsCode), new(model.Member))
 	if err != nil {
 		return nil, err
 	}
 
 	orm := new(Orm)
 	orm.Engine = engine
-
 	DbEngine = orm
 	return orm, nil
 }
